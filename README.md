f40f3c017331dd9dd81e097bb66b97bc27d53715
freemany <freeman_yam@hotmail.com>
2019-11-22 23:13:13 +1100

    more tests

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-## jDoh.PostMessage ##
+## jDoh.PostMessage
 
 A library for iframes to communicate in PostMessage with callbacks and types which is Ask & Answer responding type. 
+
+###How to run the example to see it in action
+
+$ cd examples/iframe
+$ php -S localhost:9999
+
+$ cd examples/parent
+$ php -S localhost:8888
+
+Open http://localhost:8888 
+
+
+
--- test/unit/postmessage.js
+++ test/unit/postmessage.js
@@ -36,4 +36,53 @@ describe('PostMessage', () => {
         expect(result.channel).to.eql(channel);
         expect(result.type).to.eql('ask');
     });
+
+    it('_setMessage with object', () => {
+        const message = {foo:'foo', bar:'bar'};
+        const p = new PostMessage();
+
+        expect(p._setMessage(message)).to.eql(JSON.stringify(message));
+    });
+
+    it('_setMessage with string', () => {
+        const message = 'string';
+        const p = new PostMessage();
+
+        const expected = {};
+        expected[p.msgKey] = message;
+        const result = p._setMessage(message);
+
+        expect(result).to.eql(JSON.stringify(expected));
+    });
+
+    it('_getMessage with object', () => {
+        const p = new PostMessage();
+
+        const message = 'message';
+        const obj = {};
+        obj[p.msgKey] = message;
+        const result = JSON.stringify(obj);
+
+        expect(p._getMessage(result)).to.eql(message);
+    });
+
+    it('_getMessage with object with invalid key', () => {
+        const p = new PostMessage();
+
+        const message = 'message';
+        const obj = {};
+        obj[p.msgKey] = message;
+        obj.foo = 'foo';
+        const result = JSON.stringify(obj);
+
+        expect(p._getMessage(result)).to.eql(obj);
+    });
+
+    it('_getMessage with string', () => {
+        const p = new PostMessage();
+
+        const message = 'message';
+
+        expect(p._getMessage(message)).to.eql(message);
+    });
 });
